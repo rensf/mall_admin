90e6db4b8d6714d5e45dd860dfb34ed9759912b5
rensf <2636618553@qq.com>
2021-09-02 16:11:01 +0800

    用户管理界面

--- src/view/user/user-manage.vue
+++ src/view/user/user-manage.vue
@@ -5,10 +5,16 @@
         <Input class="action-250"></Input>
         <Button class="action-btn"></Button>
         <Button class="action-btn"></Button>
-      </div>      
+      </div>
     </div>
     <div class="content">
-      <Table></Table>
+      <Table :cloumns="cloumns" :data="data" border></Table>
+      <Page
+        class="page"
+        :page-size="queryForm.size"
+        :total="queryForm.total"
+        :current="queryForm.current"
+      ></Page>
     </div>
   </div>
 </template>
@@ -19,15 +25,17 @@ export default {
     return {
       cloumns: [],
       data: [],
-    }
+      queryForm: {
+        size: 10,
+        total: 0,
+        current: 1,
+      },
+    };
   },
   mounted() {},
-  methods: {
-
-  }
-}
+  methods: {},
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
